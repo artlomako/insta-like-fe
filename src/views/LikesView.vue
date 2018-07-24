0e8410801da114383627101543027d83fd4a6aca
alomako <lomako.artur@gmail.com>
2018-07-24 21:51:48 +0200

    Remove time slider from LikesView

--- src/views/LikesView.vue
+++ src/views/LikesView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="likes-view">
     <v-slider title="Liczba akcji:" :min="50" :max="2000" :value="123"/>
-    <v-slider title="Odstęp czasowy (sek):" :max="100" :value="456"/>
     <router-view/>
   </div>
 </template>
