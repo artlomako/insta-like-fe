1b82d7e2e85f206faf809e3cafa418ca4ae47ccd
alomako <lomako.artur@gmail.com>
2018-07-24 22:15:40 +0200

    Remove redundant styles

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,26 +5,25 @@
 </template>
 
 <script>
-export default {
-  name: "TheHeader"
-};
+  export default {
+    name: "TheHeader"
+  };
 </script>
 
-<style scoped>
-.header {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
+<style>
+  .header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 
-.header--gradient {
-  /* background-image: linear-gradient(to top right, #2c2c54, #34ace0); */
-  background-color: #006d8e;
-  box-shadow: 0 3px 4px 2px #006d8e;
-}
+  .header--gradient {
+    background-color: #006d8e;
+    box-shadow: 0 3px 4px 2px #006d8e;
+  }
 
-.header__logo {
-  height: 5rem;
-  margin: 1rem 0 1rem 0;
-}
+  .header__logo {
+    height: 5rem;
+    margin: 1rem 0 1rem 0;
+  }
 </style>
