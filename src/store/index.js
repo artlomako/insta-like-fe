268356581a8cb8c3b225161698feb77bbc676a24
alomako <lomako.artur@gmail.com>
2018-08-12 20:18:41 +0200

    Remove unused service module

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import service from "./serviceModule";
 import worker from "./workerModule";
 import admin from "./adminModule";
 import createPersistedState from "vuex-persistedstate";
@@ -11,7 +10,6 @@ export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   modules: {
     admin,
-    service,
     worker
   },
   plugins: [createPersistedState()]
